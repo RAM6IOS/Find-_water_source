d5c6c3f44ecbd80c0a02e59891d80ad85e80ccaf
RAM6IOS <ios191169@gmail.comm>
2022-04-09 18:26:21 +0100

    Initial Commit

---
+++ source/sourceApp.swift
@@ -0,0 +1,17 @@
+//
+//  sourceApp.swift
+//  source
+//
+//  Created by Bouchedoub Rmazi on 9/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct sourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
